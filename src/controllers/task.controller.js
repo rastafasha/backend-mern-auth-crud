9c2c99dd6cadb69d8b07aaa1c8303492e937598a
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2025-05-14 07:18:10 -0400

    Sort tasks by creation date in getTasks function

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -3,6 +3,8 @@ import Task from '../models/task.model.js';
 export const getTasks = async (req, res) =>{
     try {
         const tasks = await Task.find()
+        //traemos las tareas en orden de ultima fecha
+        tasks.sort((a,b) => b.createdAt - a.createdAt);
         res.json(tasks);
     } catch (error) {
         return res.status(500).json({message: 'Error al obtener tareas'})
